e2582d02f1fa24506d49cbc45d966560370fcfee
karan <karanc4143@gmail.com>
2025-09-20 15:28:25 +0530

    working on query methods

--- src/main/java/com/karan/test/mongo_learn/repository/OrderRepository.java
+++ src/main/java/com/karan/test/mongo_learn/repository/OrderRepository.java
@@ -4,6 +4,10 @@ import com.karan.test.mongo_learn.entity.Order;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface OrderRepository   extends MongoRepository<Order, String> {
+    List<Order> findByStatus(String  status);
+    List<Order> findByStatusAndQuantityGreaterThan(String  status , Integer  quantity);
 }
--- src/test/java/com/karan/test/mongo_learn/MongoLearnApplicationTests.java
+++ src/test/java/com/karan/test/mongo_learn/MongoLearnApplicationTests.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import com.karan.test.mongo_learn.repository.OrderRepository;
 
+import java.util.List;
+
 @SpringBootTest
 class MongoLearnApplicationTests {
     @Autowired
@@ -16,12 +18,19 @@ class MongoLearnApplicationTests {
     void   createOrder(){
         Order  order = Order.builder()
                 .status("pending ")
-                .quantity(3)
-                .totalPrice(980)
+                .quantity(5)
+                .totalPrice(340)
                 .build();
 
         order =  repository.insert(order);
         System.out.println(order);
+    }
+
+    @Test
+     public  void  testGetOrder(){
+        List<Order>  orders=  repository.findByStatusAndQuantityGreaterThan("pending ",3);
+//        List<Order>  orders=  repository.findByStatus("pending ");
+        orders.forEach(System.out::println);
     }
 
 }
