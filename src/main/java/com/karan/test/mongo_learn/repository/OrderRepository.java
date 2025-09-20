615a14a7c036edf09ba8efd0ffb398d95f655bad
karan <karanc4143@gmail.com>
2025-09-20 15:42:18 +0530

    2 custom query methods

--- src/main/java/com/karan/test/mongo_learn/repository/OrderRepository.java
+++ src/main/java/com/karan/test/mongo_learn/repository/OrderRepository.java
@@ -2,12 +2,22 @@ package com.karan.test.mongo_learn.repository;
 
 import com.karan.test.mongo_learn.entity.Order;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface OrderRepository   extends MongoRepository<Order, String> {
     List<Order> findByStatus(String  status);
     List<Order> findByStatusAndQuantityGreaterThan(String  status , Integer  quantity);
+    List<Order> findByStatusAndQuantityGreaterThanOrderByCreatedDateDesc(String  status , Integer  quantity);
+
+    @Query("{ 'status': ?0, 'totalPrice': { $gte: ?1 } }")
+    List<Order> findPendingOrdersAbovePrice(String status, double minPrice);
+
+    @Query("{ 'createdAt': { $gte: ?0, $lt: ?1 } }")
+    List<Order> findOrdersInDateRange(LocalDateTime start, LocalDateTime end);
+
 }
--- src/test/java/com/karan/test/mongo_learn/MongoLearnApplicationTests.java
+++ src/test/java/com/karan/test/mongo_learn/MongoLearnApplicationTests.java
@@ -3,10 +3,13 @@ package com.karan.test.mongo_learn;
 
 import com.karan.test.mongo_learn.entity.Order;
 import org.junit.jupiter.api.Test;
+import org.mockito.internal.matchers.Or;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import com.karan.test.mongo_learn.repository.OrderRepository;
 
+import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.List;
 
 @SpringBootTest
@@ -28,8 +31,18 @@ class MongoLearnApplicationTests {
 
     @Test
      public  void  testGetOrder(){
-        List<Order>  orders=  repository.findByStatusAndQuantityGreaterThan("pending ",3);
+//        List<Order>  orders=  repository.findByStatusAndQuantityGreaterThan("pending ",3);
+        List<Order>  orders=  repository.findByStatusAndQuantityGreaterThanOrderByCreatedDateDesc("pending ",3);
 //        List<Order>  orders=  repository.findByStatus("pending ");
+        orders.forEach(System.out::println);
+    }
+
+    @Test
+    public  void  CustomeQuery(){
+//        List<Order> orders =  repository.findPendingOrdersAbovePrice("pending " ,200);
+        LocalDateTime startDate = LocalDateTime.of(2025, 9, 20, 0, 0);
+        LocalDateTime endDate = LocalDateTime.of(2025, 9, 20, 23, 59, 59,20);
+        List<Order> orders = repository.findOrdersInDateRange(startDate, endDate);
         orders.forEach(System.out::println);
     }
 
